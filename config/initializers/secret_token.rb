7cb05b194b24a04008754946858084bdc2c7422e
Aayush Iyer <aayush.iyer@gmail.com>
2012-11-04 16:28:35 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunrise::Application.config.secret_token = '3dc4091091a893c4e8b20fb8eefef5f08eb4bcc37218a508bd1dadf28d85ecb94e7ddea3ef1b020e084a4af65101b3a1f009f1f92065b73f8ad54aa16cb94011'
